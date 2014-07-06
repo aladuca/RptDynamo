8a970f7735f57d1e2b81e5ed0b52b5229df7ed10
Adrian La Duca <aladuca@summatus.net>
2014-07-06 16:06:22 -0400

    Successful Job Processing
    Added class to handle job parameters.
    Added command line parameters.

--- RptDynamo/Program.cs
+++ RptDynamo/Program.cs
@@ -1,8 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection;
+using System.Runtime.Serialization.Json;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+using CommandLine;
+using CommandLine.Text;
+
+using CrystalDecisions.CrystalReports.Engine;
+using CrystalDecisions.Shared;
+
 
 namespace RptDynamo
 {
@@ -10,6 +24,96 @@ namespace RptDynamo
     {
         static void Main(string[] args)
         {
+            var options = new Options();
+            if (CommandLine.Parser.Default.ParseArguments(args, options))
+            {
+                // consume Options instance properties
+                if (options.Verbose)
+                {
+                    Console.WriteLine(options.config);
+                    Console.WriteLine(options.job);
+                    string json = File.ReadAllText(options.job);
+                    DataContractJsonSerializer jobser = new DataContractJsonSerializer(typeof(RptJob));
+                    StreamReader file = File.OpenText(options.job);
+                    Stream stream = File.OpenRead(options.job);
+                    RptJob job = (RptJob)jobser.ReadObject(stream);
+                    ProcessRpt(job);
+                }
+                else
+                    Console.WriteLine("working ...");
+            }
+        }
+        static void ProcessRpt(RptJob rptJob)
+        {
+            ReportDocument rpt = new ReportDocument();
+
+            // Open Report
+            rpt.Load(rptJob.report.Filename);
+            Trace.WriteLine("Loaded Report");
+
+            // Pass Parameters to Report
+            foreach (Parameter rptParam in rptJob.report.parameter)
+            {
+                rpt.SetParameterValue(rptParam.Name, rptParam.text);
+            }
+
+            // Create temporary output directory
+            string tempdir = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+            Directory.CreateDirectory(tempdir);
+            tempdir += "\\";
+            Console.WriteLine("Created tmpdir " + tempdir);
+
+            // Determine File Format and Output name
+            string outfile = null;
+            ExportFormatType crformat = ExportFormatType.NoFormat;
+            switch (rptJob.report.output)
+            {
+                case "xlsx":
+                    outfile = tempdir + Path.ChangeExtension(Path.GetFileName(rptJob.report.Filename), ".xlsx");
+                    crformat = ExportFormatType.ExcelWorkbook;
+                    break;
+                case "xls":
+                    outfile = tempdir + Path.ChangeExtension(Path.GetFileName(rptJob.report.Filename), ".xls");
+                    crformat = ExportFormatType.Excel;
+                    break;
+                case "pdf":
+                    outfile = tempdir + Path.ChangeExtension(Path.GetFileName(rptJob.report.Filename), ".pdf");
+                    crformat = ExportFormatType.PortableDocFormat;
+                    break;
+                case "csv":
+                    outfile = tempdir + Path.ChangeExtension(Path.GetFileName(rptJob.report.Filename), ".csv");
+                    crformat = ExportFormatType.CharacterSeparatedValues;
+                    break;
+            }
+
+            // Generate Report
+            rpt.ExportToDisk(crformat, outfile);
+
+            // Clean up Crystal Reports ReportDocument
+            rpt.Close();
+            rpt.Dispose();
+        }
+    }
+    class Options
+    {
+        [Option('c', "config", Required = true, HelpText = "Specify Configuration File.")]
+        public string config { get; set; }
+
+        [Option('j', "job", Required = false, HelpText = "Specify Job File.")]
+        public string job { get; set; }
+
+        [Option('v', null, HelpText = "Print details during execution.")]
+        public bool Verbose { get; set; }
+
+        [HelpOption]
+        public string GetUsage()
+        {
+            // this without using CommandLine.Text
+            //  or using HelpText.AutoBuild
+            var usage = new StringBuilder();
+            usage.AppendLine("Quickstart Application 1.0");
+            usage.AppendLine("Read user manual for usage instructions...");
+            return usage.ToString();
         }
     }
 }
---
+++ RptDynamo/RptJob.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RptDynamo
+{
+
+    public class Parameter
+    {
+        public string Name { get; set; }
+        public string MultipleValues { get; set; }
+        public object text { get; set; }
+    }
+
+    public class Database
+    {
+        public string type { get; set; }
+        public string resource { get; set; }
+        public string password { get; set; }
+    }
+
+    public class Report
+    {
+        public string Filename { get; set; }
+        public List<Parameter> parameter { get; set; }
+        public Database database { get; set; }
+        public string output { get; set; }
+    }
+
+    public class Email
+    {
+        public List<string> to { get; set; }
+        public List<string> cc { get; set; }
+    }
+
+    public class RptJob
+    {
+        public Report report { get; set; }
+        public Email email { get; set; }
+    }
+
+}
